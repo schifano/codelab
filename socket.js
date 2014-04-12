ab2bc5d9eae7daa29b044637ae2b059a407f65e5
Austin <email@austinsavage.me>
2014-04-12 02:43:34 -0500

    added code to send specific sockets messages

--- socket.js
+++ socket.js
@@ -1,8 +1,12 @@
 var io = require('socket.io').listen(80);
 
+var users = [];
+
 io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+
+  users.push(socket.id);
+
+  io.sockets.socket(socket.id).emit('socket-id', {'Socket ID': socket.id});
+
+
 });
