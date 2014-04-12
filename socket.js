c84425ed92e76ee0fde81f55d4fbe2fec5a5d2b3
Rachel Schifano <rschifa1@gmail.com>
2014-04-12 01:05:27 -0500

    Creater users and code html sections

--- socket.js
+++ socket.js
@@ -1,1 +1,8 @@
-var io = require('socket.io').listen(8080);
+var io = require('socket.io').listen(80);
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
