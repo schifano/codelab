ef98d7a70c211098b04402fbc9aa3f4811c267ae
n <kevin@ubuntu.ubuntu-domain>
2014-04-12 11:39:39 -0500

    i dont remember

--- socket2.js
+++ socket2.js
@@ -1,4 +1,4 @@
-var io = require('socket.io').listen(80);
+var io = require('socket.io').listen(8080);
 
 var users = [];
 
@@ -7,7 +7,7 @@ io.sockets.on('connection', function (socket) {
   user.socketID = socket.id;
   users.push(user);
 
-  io.sockets.socket(socket.id).emit('socket-id', {'Socket ID': socket.id});
+  io.sockets.socket(socket.id).emit('session-id', {'Socket ID': socket.id});
   
   for(var i = 0; i < users.length; i++) {
 	console.log(users[i].socketID);
@@ -15,10 +15,19 @@ io.sockets.on('connection', function (socket) {
   //console.log(users);
   console.log(users.length);
 
+socket.on('update-user', function(data) {
+	var user = new Object();
+	
+	user.text_area = data;
+	console.log(data.text_area);
+	console.log(data.session_id);
+	console.log("here");
+
+	for(var i = 0; i < users.length; i++) {
+		if(users[i].session_id == data.session_id) {
+			users[i] = data.text_area;
+		}
+	}
+
 });
-
-io.sockets.on('textarea', function(socket) {
-
-
-
-}
+});
