911f2a8ea17b95aaf76f9c5af51127d099610a42
Rachel Schifano <rschifa1@gmail.com>
2014-04-13 06:51:47 -0500

    Added content to frontend

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,210 @@
-!function(){"use strict";var e="undefined"!=typeof window?window:global;if("function"!=typeof e.require){var n={},r={},i=function(e,n){return{}.hasOwnProperty.call(e,n)},t=function(e,n){var r,i,t=[];r=/^\.\.?(\/|$)/.test(n)?[e,n].join("/").split("/"):n.split("/");for(var o=0,u=r.length;u>o;o++)i=r[o],".."===i?t.pop():"."!==i&&""!==i&&t.push(i);return t.join("/")},o=function(e){return e.split("/").slice(0,-1).join("/")},u=function(n){return function(r){var i=o(n),u=t(i,r);return e.require(u,n)}},s=function(e,n){var i={id:e,exports:{}};return r[e]=i,n(i.exports,u(e),i),i.exports},a=function(e,o){var u=t(e,".");if(null==o&&(o="/"),i(r,u))return r[u].exports;if(i(n,u))return s(u,n[u]);var a=t(u,"./index");if(i(r,a))return r[a].exports;if(i(n,a))return s(a,n[a]);throw new Error('Cannot find module "'+e+'" from "'+o+'"')},f=function(e,r){if("object"==typeof e)for(var t in e)i(e,t)&&(n[t]=e[t]);else n[e]=r},c=function(){var e=[];for(var r in n)i(n,r)&&e.push(r);return e};e.require=a,e.require.define=f,e.require.register=f,e.require.list=c,e.require.brunch=!0}}(),require.register("initialize",function(){$(document).ready(function(){var e={};e.nickname="anonymous",e.textarea="";var n=io.connect("http://:8080");n.on("session-id",function(r){e.session_id=r.session_id,n.emit("update-user",e),n.emit("refresh-friends")}),n.on("retrieve-code",function(e){guestText.setValue(e)}),n.on("watch-code",function(e){guestText.setValue(e.textarea)}),n.on("friend-disconnect",function(){n.emit("refresh-friends")}),n.on("friends-broadcast",function(r){$("ul#users").empty();for(var i=0;i<r.length;i++)$("ul#users").append('<li id="'+r[i].session_id+'"><a href="#">'+r[i].nickname+"</a></li>");$("#users li").on("click",function(r){r.preventDefault(),e.partner_id=this.id,n.emit("update-user",e),n.emit("fetch-code",e.partner_id)})}),$("div.CodeMirror.cm-s-mdn-like").on("change keydown keyup paste",function(){e.textarea=userText.getValue(),n.emit("update-user",e)}),$("#nicknameForm").submit(function(){return e.nickname=$(this).children("#nicknameText").val(),n.emit("update-user",e),n.emit("refresh-friends"),!1})})});
+(function(/*! Brunch !*/) {
+  'use strict';
+
+  var globals = typeof window !== 'undefined' ? window : global;
+  if (typeof globals.require === 'function') return;
+
+  var modules = {};
+  var cache = {};
+
+  var has = function(object, name) {
+    return ({}).hasOwnProperty.call(object, name);
+  };
+
+  var expand = function(root, name) {
+    var results = [], parts, part;
+    if (/^\.\.?(\/|$)/.test(name)) {
+      parts = [root, name].join('/').split('/');
+    } else {
+      parts = name.split('/');
+    }
+    for (var i = 0, length = parts.length; i < length; i++) {
+      part = parts[i];
+      if (part === '..') {
+        results.pop();
+      } else if (part !== '.' && part !== '') {
+        results.push(part);
+      }
+    }
+    return results.join('/');
+  };
+
+  var dirname = function(path) {
+    return path.split('/').slice(0, -1).join('/');
+  };
+
+  var localRequire = function(path) {
+    return function(name) {
+      var dir = dirname(path);
+      var absolute = expand(dir, name);
+      return globals.require(absolute, path);
+    };
+  };
+
+  var initModule = function(name, definition) {
+    var module = {id: name, exports: {}};
+    cache[name] = module;
+    definition(module.exports, localRequire(name), module);
+    return module.exports;
+  };
+
+  var require = function(name, loaderPath) {
+    var path = expand(name, '.');
+    if (loaderPath == null) loaderPath = '/';
+
+    if (has(cache, path)) return cache[path].exports;
+    if (has(modules, path)) return initModule(path, modules[path]);
+
+    var dirIndex = expand(path, './index');
+    if (has(cache, dirIndex)) return cache[dirIndex].exports;
+    if (has(modules, dirIndex)) return initModule(dirIndex, modules[dirIndex]);
+
+    throw new Error('Cannot find module "' + name + '" from '+ '"' + loaderPath + '"');
+  };
+
+  var define = function(bundle, fn) {
+    if (typeof bundle === 'object') {
+      for (var key in bundle) {
+        if (has(bundle, key)) {
+          modules[key] = bundle[key];
+        }
+      }
+    } else {
+      modules[bundle] = fn;
+    }
+  };
+
+  var list = function() {
+    var result = [];
+    for (var item in modules) {
+      if (has(modules, item)) {
+        result.push(item);
+      }
+    }
+    return result;
+  };
+
+  globals.require = require;
+  globals.require.define = define;
+  globals.require.register = define;
+  globals.require.list = list;
+  globals.require.brunch = true;
+})();
+require.register("initialize", function(exports, require, module) {
+/*
+
+   ___          _        __       _     
+  / __\___   __| | ___  / /  __ _| |__  
+ / /  / _ \ / _` |/ _ \/ /  / _` | '_ \ 
+/ /__| (_) | (_| |  __/ /__| (_| | |_) |
+\____/\___/ \__,_|\___\____/\__,_|_.__/ 
+                                        
+
+*/
+
+// We wait for the document to finish loading.
+$(document).ready(function() {
+
+  // Let's create the user object.
+  var user = {};
+      user.nickname = "anonymous"; // Default nickname
+      user.textarea = "";          // Default text file
+
+  // Now open the socket connection
+  var socket = io.connect('http://:8080');
+
+  //
+  //  Socket.io Listeners/Events
+  //
+
+  // Fired when the server passes the client a session id.
+  socket.on('session-id', function (data) {
+
+    user.session_id = data.session_id;
+
+    socket.emit('update-user', user);
+
+    socket.emit('refresh-friends');
+
+  });
+
+  // Fired when the server responds with partner code.
+  socket.on('retrieve-code', function(textarea) {
+    
+    guestText.setValue(textarea);
+
+  });
+
+  // Fired when the server recieves an update from a partner.
+  socket.on('watch-code', function(partnerUser) {
+
+    guestText.setValue(partnerUser.textarea);
+  
+  });
+
+  // Fired when a friend disconnects.
+  socket.on('friend-disconnect', function () {
+
+      socket.emit('refresh-friends');
+  });
+
+  // Fired when the server broadcasts the current list of online friends.
+  socket.on('friends-broadcast', function (data) {
+
+    $('ul#users').empty();
+
+    for(var i = 0; i < data.length; i++){
+
+      $('ul#users').append( "<li id=\"" + data[i].session_id + "\"><a href=\"#\">" + data[i].nickname + "</a></li>");
+
+    }
+
+    $("#users li").on('click', function(e) {
+
+      e.preventDefault();
+
+      user.partner_id = this.id;
+
+      socket.emit('update-user', user);
+
+      socket.emit('fetch-code', user.partner_id); 
+
+    });
+
+  });
+
+  //
+  //  jQuery Listeners/Events
+  //
+
+  $("div.CodeMirror.cm-s-mdn-like").on("change keydown keyup paste", function() {
+
+      user.textarea = userText.getValue();
+
+      socket.emit('update-user', user);
+
+  });
+
+
+  $("#nicknameForm").submit(function() {
+
+    user.nickname = $(this).children("#nicknameText").val();
+
+    socket.emit('update-user', user);
+
+    socket.emit('refresh-friends');
+
+    return false;
+  
+  });
+
+});
+
+
+
+
+
+});
+
+;
+//# sourceMappingURL=app.js.map
